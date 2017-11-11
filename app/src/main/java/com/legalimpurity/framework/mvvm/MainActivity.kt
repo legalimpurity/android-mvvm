c6253d16422acb81c27dce7d97d5038dc3bd6597
rajatkhanna <legalimpurity@gmail.com>
2017-11-11 22:50:02 +0530

    Initial commit, Blank project made from Kotlin.

---
+++ app/src/main/java/com/legalimpurity/framework/mvvm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.legalimpurity.framework.mvvm
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
