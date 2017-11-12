2f27bac736b99ffef6fd2210ce2523827781e78e
rajatkhanna <legalimpurity@gmail.com>
2017-11-12 11:04:57 +0530

    Lets start building DI code structure, build a package di followed by a package modules inside it and create and AppModule.

---
+++ app/src/main/java/com/legalimpurity/framework/mvvm/di/modules/AppModule.kt
@@ -0,0 +1,15 @@
+package com.legalimpurity.framework.mvvm.di.modules
+
+import com.legalimpurity.framework.mvvm.MVVMApp
+import dagger.Module
+import dagger.Provides
+
+/**
+ * Created by rajatkhanna on 12/11/17.
+ */
+
+@Module
+class AppModule(val app: MVVMApp) {
+    @Provides
+    fun provideApp(): MVVMApp = app
+}
